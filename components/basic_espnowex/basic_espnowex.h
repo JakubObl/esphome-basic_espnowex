bfafae0ec0646a14b2d0124377a1ca42c8f4e52e
JakubObl <jakub.obligacja@gmail.com>
2025-05-20 00:49:39 +0200

    Update basic_espnowex.h

--- components/basic_espnowex/basic_espnowex.h
+++ components/basic_espnowex/basic_espnowex.h
@@ -12,15 +12,15 @@ namespace espnow {
 
 class BasicESPNowEx;
 
-class OnMessageTrigger : public ::esphome::automation::Trigger<std::vector<uint8_t>, std::array<uint8_t, 6>> {
+class OnMessageTrigger : public ::esphome::Trigger<std::vector<uint8_t>, std::array<uint8_t, 6>> {
  public:
   explicit OnMessageTrigger(BasicESPNowEx *parent);
 };
-class OnRecvAckTrigger : public ::esphome::automation::Trigger<std::array<uint8_t, 6>> {
+class OnRecvAckTrigger : public ::esphome::Trigger<std::array<uint8_t, 6>> {
 public:
     explicit OnRecvAckTrigger(BasicESPNowEx *parent);
 };
-class OnRecvCmdTrigger : public ::esphome::automation::Trigger<std::array<uint8_t, 6>, int16_t> {
+class OnRecvCmdTrigger : public ::esphome::Trigger<std::array<uint8_t, 6>, int16_t> {
 public:
     explicit OnRecvCmdTrigger(BasicESPNowEx *parent);
 };
