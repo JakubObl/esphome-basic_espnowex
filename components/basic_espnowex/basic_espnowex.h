8a989a037357f84bc13fcb8de9a9f395af2bda02
JakubObl <jakub.obligacja@gmail.com>
2025-05-22 21:17:33 +0200

    Update basic_espnowex.h

--- components/basic_espnowex/basic_espnowex.h
+++ components/basic_espnowex/basic_espnowex.h
@@ -11,9 +11,32 @@ namespace esphome {
 namespace espnow {
 
 class BasicESPNowEx;
-class OnMessageTrigger;
-class OnRecvAckTrigger;
-class OnRecvCmdTrigger;
+
+
+class OnMessageTrigger : public ::esphome::Trigger<const std::vector<uint8_t>, const std::array<uint8_t, 6>>, public Component {
+ public:
+  explicit OnMessageTrigger(BasicESPNowEx *parent){
+                parent->add_on_message_callback([this](const std::vector<uint8_t> message, const std::array<uint8_t, 6> mac) {
+                    trigger(message, mac);
+                });
+            }
+};
+class OnRecvAckTrigger : public ::esphome::Trigger<const std::array<uint8_t, 6>>, public Component {
+public:
+    explicit OnRecvAckTrigger(BasicESPNowEx *parent){
+                parent->add_on_recv_ack_callback([this](const std::array<uint8_t, 6> mac) {
+                    trigger(mac);
+                });
+            }
+};
+class OnRecvCmdTrigger : public ::esphome::Trigger<const std::array<uint8_t, 6>, const int16_t>, public Component {
+public:
+    explicit OnRecvCmdTrigger(BasicESPNowEx *parent){
+                parent->add_on_recv_cmd_callback([this](const std::array<uint8_t, 6> mac, const int16_t cmd) {
+                    trigger(mac, cmd);
+                });
+            }
+};
 
 class BasicESPNowEx : public Component {
  public:
@@ -62,32 +85,6 @@ class BasicESPNowEx : public Component {
   
 
 };
-
-class OnMessageTrigger : public ::esphome::Trigger<const std::vector<uint8_t>, const std::array<uint8_t, 6>>, public Component {
- public:
-  explicit OnMessageTrigger(BasicESPNowEx *parent){
-                parent->add_on_message_callback([this](const std::vector<uint8_t> message, const std::array<uint8_t, 6> mac) {
-                    trigger(message, mac);
-                });
-            }
-};
-class OnRecvAckTrigger : public ::esphome::Trigger<const std::array<uint8_t, 6>>, public Component {
-public:
-    explicit OnRecvAckTrigger(BasicESPNowEx *parent){
-                parent->add_on_recv_ack_callback([this](const std::array<uint8_t, 6> mac) {
-                    trigger(mac);
-                });
-            }
-};
-class OnRecvCmdTrigger : public ::esphome::Trigger<const std::array<uint8_t, 6>, const int16_t>, public Component {
-public:
-    explicit OnRecvCmdTrigger(BasicESPNowEx *parent){
-                parent->add_on_recv_cmd_callback([this](const std::array<uint8_t, 6> mac, const int16_t cmd) {
-                    trigger(mac, cmd);
-                });
-            }
-};
-
 
 }  // namespace espnow
 }  // namespace esphome
