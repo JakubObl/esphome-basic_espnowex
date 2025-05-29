36cc6dca7f49bc91264c98162e7372bb43bb90cb
JakubObl <jakub.obligacja@gmail.com>
2025-05-29 22:36:29 +0200

    Update basic_espnowex.h

--- components/basic_espnowex/basic_espnowex.h
+++ components/basic_espnowex/basic_espnowex.h
@@ -91,6 +91,7 @@ class BasicESPNowEx : public Component {
   void send_espnow_str(std::string message, const std::array<uint8_t, 6> &peer_mac);
   void send_espnow(const std::vector<uint8_t> &msg, const std::array<uint8_t, 6> &peer_mac);
   void send_espnow_cmd(int16_t cmd, const std::array<uint8_t, 6> &peer_mac);
+  void clear_pending_messages();
 
   void add_on_message_trigger(OnMessageTrigger *trigger);
   void add_on_recv_ack_trigger(OnRecvAckTrigger *trigger);
