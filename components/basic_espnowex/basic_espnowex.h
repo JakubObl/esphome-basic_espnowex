21a9232eb96d384005b2be694c05e8736f93b0e2
JakubObl <jakub.obligacja@gmail.com>
2025-05-30 14:17:00 +0200

    Update basic_espnowex.h

--- components/basic_espnowex/basic_espnowex.h
+++ components/basic_espnowex/basic_espnowex.h
@@ -6,8 +6,8 @@
 #include "esp_timer.h"
 
 // FreeRTOS
-//#include "freertos/FreeRTOS.h"
-//#include "freertos/semphr.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 
 #include <array>
 #include <vector>
@@ -104,7 +104,9 @@ class BasicESPNowEx : public Component {
   void process_send_queue();
   std::vector<PendingMessage> pending_messages_;
   std::vector<ReceivedMessageInfo> received_history_;
-  //SemaphoreHandle_t queue_mutex_;;
+  SemaphoreHandle_t queue_mutex_;
+  SemaphoreHandle_t history_mutex_;
+
   esp_timer_handle_t retry_timer_;
   static void static_wifi_event(void* arg, esp_event_base_t base, int32_t id, void* data);
   static void recv_cb(const uint8_t *mac, const uint8_t *data, int len);
